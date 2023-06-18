13486d4d26c5ef4862944cf0d1d83f819ddd70fd
Scott <scottsewares@hotmail.com>
2023-06-18 15:43:49 -0700

    update to monitor.py

--- monitor.py
+++ monitor.py
@@ -6,12 +6,26 @@ import requests
 import time
 import sched
 import sys
+import logging
 from icmplib import multiping
 
 # Global vars
+# Using a json file to import elements for the database connection.
+db_dbname = json.loads(open('sql.json', 'r').read())['sql']['pg_dbname']
+db_name = json.loads(open('sql.json', 'r').read())['sql']['pg_user']
 db_pass = json.loads(open('sql.json', 'r').read())['sql']['pg_pass']
+db_host = json.loads(open('sql.json', 'r').read())['sql']['pg_host']
+#db_port = json.loads(open('sql.json', 'r').read())['sql']['pg_port']
+# Date and time used for timestamp in the database.
 t = time.strftime("%Y/%m/%d %H:%M:%S %Z")
+# Time deplay between testing nodes and urls.
 sch_delay = 10
+
+# Logging
+logging.basicConfig(filename='my.log',
+                    filemode='a',
+                    format='%(asctime)s, %(levelname)s %(message)s',
+                    level=logging.DEBUG)
 
 # Test the connectivity of nodes
 def ping_nodes():  
@@ -24,11 +38,11 @@ def ping_nodes():
     
     for p in mp:
         if p.is_alive:
-            print(f'{p.address} is up')
+            print(f'{p.address} is up') # for debug
             pg_cur.execute(f"UPDATE nodes SET status = 'up' WHERE ip_addr = '{p.address}'")
             pg_cur.execute(f"UPDATE nodes SET timestamp = '{t}' WHERE ip_addr = '{p.address}'")
         else:
-            print(f'{p.address} is down')
+            print(f'{p.address} is down') # for debug
             pg_cur.execute(f"UPDATE nodes SET status = 'down' WHERE ip_addr = '{p.address}'")
             pg_cur.execute(f"UPDATE nodes SET timestamp = '{t}' WHERE ip_addr = '{p.address}'")
 
@@ -44,15 +58,15 @@ def url_check():
         try:
             r = requests.get(i)
             if r.status_code == 200:
-                print('up')
+                print('up') # for debug
                 pg_cur.execute(f"UPDATE websites SET status = 'up' WHERE url = '{i}'")
                 pg_cur.execute(f"UPDATE websites SET timestamp = '{t}' WHERE url = '{i}'")
             else:
-                print('down')
+                print('down') # for debug
                 pg_cur.execute(f"UPDATE websites SET status = 'down' WHERE url = '{i}'")
                 pg_cur.execute(f"UPDATE websites SET timestamp = '{t}' WHERE url = '{i}'")
         except requests.ConnectionError:
-            print(f'connection fail on {i}')
+            print(f'connection fail on {i}') # for debug
             pg_cur.execute(f"UPDATE websites SET status = 'dns error' WHERE url = '{i}'")
             pg_cur.execute(f"UPDATE websites SET timestamp = '{t}' WHERE url = '{i}'")
     
@@ -68,7 +82,7 @@ if __name__ == "__main__":
     # Test connection to database server. The program will exit if it cannot conenct to the database or 
     # if username/password is not correct.
     try:
-        conn = psycopg2.connect(dbname='monitor', user='sqluser', host='192.168.64.14', password='test')
+        conn = psycopg2.connect(dbname=db_dbname, user=db_name, host=db_host, password=db_pass)
         pg_cur = conn.cursor()
     except psycopg2.OperationalError:
         sys.exit("Failed to connect to DB. Please check if the network connection or username/password is correct.")
