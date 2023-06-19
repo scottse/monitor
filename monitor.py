d888c7fba70758fe26e679eaa16e82db7315ea8e
Scott <scottsewares@hotmail.com>
2023-06-18 19:46:40 -0700

    Updates to monitor.py

--- monitor.py
+++ monitor.py
@@ -19,16 +19,10 @@ db_host = json.loads(open('sql.json', 'r').read())['sql']['pg_host']
 # Date and time used for timestamp in the database.
 t = time.strftime("%Y/%m/%d %H:%M:%S %Z")
 # Time deplay between testing nodes and urls.
-sch_delay = 10
-
-# Logging
-logging.basicConfig(filename='my.log',
-                    filemode='a',
-                    format='%(asctime)s, %(levelname)s %(message)s',
-                    level=logging.DEBUG)
+sch_delay = 300
 
 # Test the connectivity of nodes
-def ping_nodes():  
+def ping_nodes():
     pg_cur.execute("SELECT ip_addr FROM nodes")
     fetch_from_db = pg_cur.fetchall()
     node_list = [x[0] for x in fetch_from_db]
@@ -38,13 +32,19 @@ def ping_nodes():
     
     for p in mp:
         if p.is_alive:
-            print(f'{p.address} is up') # for debug
+            #print(f'{p.address} is up') # for debug
+            # Updates the database with the status of 'up' and it also adds a timestamp.
             pg_cur.execute(f"UPDATE nodes SET status = 'up' WHERE ip_addr = '{p.address}'")
+            # Updates the database with the status of 'up' and it also adds a timestamp.
             pg_cur.execute(f"UPDATE nodes SET timestamp = '{t}' WHERE ip_addr = '{p.address}'")
         else:
-            print(f'{p.address} is down') # for debug
+            #print(f'{p.address} is down') # for debug
+            # Updates the database with the status of 'down' and it also adds a timestamp.
             pg_cur.execute(f"UPDATE nodes SET status = 'down' WHERE ip_addr = '{p.address}'")
             pg_cur.execute(f"UPDATE nodes SET timestamp = '{t}' WHERE ip_addr = '{p.address}'")
+            
+            # Error logging
+            logging.warning(f'{p.address} is down')
 
     conn.commit()
 
@@ -58,17 +58,23 @@ def url_check():
         try:
             r = requests.get(i)
             if r.status_code == 200:
-                print('up') # for debug
+                #print('up') # for debug
+                # Updates the database with status of "up", and it also adds a timestamp.
                 pg_cur.execute(f"UPDATE websites SET status = 'up' WHERE url = '{i}'")
                 pg_cur.execute(f"UPDATE websites SET timestamp = '{t}' WHERE url = '{i}'")
             else:
-                print('down') # for debug
+                #print('down') # for debug
+                # Updates the database with status of 'down', and it also adds a timestamp.
                 pg_cur.execute(f"UPDATE websites SET status = 'down' WHERE url = '{i}'")
                 pg_cur.execute(f"UPDATE websites SET timestamp = '{t}' WHERE url = '{i}'")
+                
+                # Error logging
+                logging.warning(f'{i} did not have http status 200')
         except requests.ConnectionError:
-            print(f'connection fail on {i}') # for debug
+            #print(f'connection fail on {i}') # for debug
             pg_cur.execute(f"UPDATE websites SET status = 'dns error' WHERE url = '{i}'")
             pg_cur.execute(f"UPDATE websites SET timestamp = '{t}' WHERE url = '{i}'")
+            logging.warning(f'{i} failed to connect. DNS?')
     
     conn.commit()
 
@@ -79,14 +85,21 @@ def main():
     url_check()
 
 if __name__ == "__main__":
+    # Enable logging
+    logging.basicConfig(filename='monitor.log',
+            filemode='a',
+            format='%(asctime)s, %(levelname)s %(message)s',
+            level=logging.WARNING)
+    
     # Test connection to database server. The program will exit if it cannot conenct to the database or 
     # if username/password is not correct.
     try:
         conn = psycopg2.connect(dbname=db_dbname, user=db_user, host=db_host, password=db_pass)
         pg_cur = conn.cursor()
     except psycopg2.OperationalError:
-        sys.exit("Failed to connect to DB. Please check if the network connection or username/password is correct.")
-    
+        logging.fatal("Unable to connect to database. Please check network connection/username/password and try again.")
+        sys.exit(1)
+
     # The sched module is being used to schedule the tasks that runs at the speified time.
     sch = sched.scheduler(time.time, time.sleep)
 
