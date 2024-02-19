2a35484b372e913d764d0a8e3c25bff961f8c4fa
Scott <scottsewares@hotmail.com>
2024-02-18 17:20:04 -0800

    Moving status functions to main file.

--- monitor.py
+++ monitor.py
@@ -7,6 +7,8 @@ import time
 import sched
 import sys
 import logging
+import pandas
+import csv
 from icmplib import multiping
 
 # Global vars
@@ -88,11 +90,73 @@ def url_check():
 
     conn.commit()
 
+# Prepares the nodes list with a csv file.
+def nodes_table():
+    pg_cur.execute("SELECT hostname,ip_addr,status,timestamp FROM nodes")
+    db_fetch = pg_cur.fetchall()
+
+    # Creates the csv file.
+    nodes_csv = open('webpage/nodes.csv', 'w')
+    cw = csv.writer(nodes_csv)
+    c_header = ['hostname', 'IP address', 'Status', 'Last Updated']
+    cw.writerow(c_header)
+    cw.writerows(db_fetch)
+    nodes_csv.close()
+
+    # Creates text file of html data from the csv.
+    p_tables = pandas.read_csv('webpage/nodes.csv')
+    p_tables.to_html('webpage/nodes.txt', index=False)
+    p_html = p_tables.to_html()
+
+# Get data related to URLs from the monitor database.
+def website_table():
+    pg_cur.execute("SELECT url,ip_addr,status,timestamp FROM websites")
+    db_fetch = pg_cur.fetchall()
+
+    # Creates text file of html data from the csv.
+    url_csv = open('webpage/url.csv', 'w')
+    cw = csv.writer(url_csv)
+    c_header = ['URL', 'IP Address', 'Status', 'Last Updated']
+    cw.writerow(c_header)
+    cw.writerows(db_fetch)
+    url_csv.close()
+
+    # Creates text file of html data from the csv.
+    p_tables = pandas.read_csv('webpage/url.csv')
+    p_tables.to_html('webpage/url.txt', index=False)
+    p_html = p_tables.to_html()
+
+# Creates a web page with the status of each item being monitor from the database.
+def status_webpage():
+    # Creates the index.html file by writing the html code into it. It pulls the html table code
+    # from texts files created from the nodes and website
+    html_file = open('/var/www/monitor/index.html', 'w')
+    html_file.write(
+        '<html>\n<head>\n<link rel="stylesheet" href="style.css">\n</head>\n<body>\n<h2>Project Monitor Status Page</h2>\n'
+    )
+    html_file.writelines('<h3>Nodes:</h3>')
+    with open('nodes.txt', 'r') as f1:
+        for l1 in f1:
+            html_file.write(l1)
+    html_file.write('<hr class="dashed">\n')
+    html_file.writelines('<h3>Websites:</h3>')
+    with open('url.txt', 'r') as f2:
+        for l2 in f2:
+            html_file.write(l2)
+    html_file.write(f'\nLast Updated: {time.strftime("%Y/%m/%d %H:%M:%S %Z")}')
+    html_file.write('\n</body>\n</html>')
+
 def main():
     # This calls the ping_nodes() function to ping all of the nodes in the database.
     ping_nodes()
     # The url_check function is used for checking the http status code of a website.
     url_check()
+    # Gets the node data from the database and stores the data as a csv file.
+    nodes_table()
+    # Gets the website data from the database and stores the data as a csv file.
+    website_table()
+    # Takes the data stored in the csv files and writes index.html on each update.
+    status_webpage()
 
 if __name__ == "__main__":
     # Enable logging
